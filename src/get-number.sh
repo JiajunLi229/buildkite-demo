b243d55abfa6d40bc65f77c952899306d115784a
JiajunLi <jiajun.li@hometrack.com.au>
2021-03-19 07:39:40 +0800

    added scripts

--- src/get-number.sh
+++ src/get-number.sh
@@ -1,5 +1,5 @@
-generated_number=$(generate_number)
+#generated_number=$(generate_number)
 
-echo "generated number is ${generate_number}"
+#echo "generated number is ${generate_number}"
 
-buildkite-agent meta-data set "generated-number" "${generated_number}"
+echo "generated number is 6666"
